9e62abb7bb9367aa7bc88a47f70d0eccba1f6d7b
noalias <563626721@qq.com>
2018-12-04 01:11:56 +0800

    update

---
+++ Chapter13/sum.c
@@ -0,0 +1,13 @@
+#include "../aheadfile.h"
+
+int main(int argc, char *argv[])
+{
+    int sum = 0;
+
+    while (*argv != NULL) 
+        sum += atoi(*argv++);
+
+    printf("Total: %d\n", sum);
+
+    return 0;
+}
--- aheadfile.h
+++ aheadfile.h
@@ -1,2 +1,3 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
