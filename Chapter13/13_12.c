9c01e83b0896fadbf253bc30ce58b4dd33d444de
noalias <563626721@qq.com>
2018-12-08 16:20:24 +0800

    update

--- Chapter13/13_12.c
+++ Chapter13/13_12.c
@@ -5,6 +5,42 @@
 
 int main(void)
 {
-    char sentence[NUM_WORD][LENGTH_WORD];
+    char sentence[NUM_WORD][LENGTH_WORD], ch;
+    int m, n;
+
+    printf("Enter a sentence: ");
+    for (m = 0; m < NUM_WORD; m++) {
+        for (n = 0; n < LENGTH_WORD; n++) {
+            sentence[m][n] = getchar();
+            if (sentence[m][n] == ' ' || sentence[m][n] == '?' || sentence[m][n] == '.' || sentence[m][n] == '!')
+                break;
+        }
+        if (sentence[m][n] == '?' || sentence[m][n] == '!' || sentence[m][n] == '.') {
+            ch = sentence[m][n];
+            sentence[m][n] = '\0';
+            break;
+        }
+        sentence[m][n] = '\0';
+    }
+
+    printf("Reversal of sentence: ");
+    while (m != 0) {
+        printf("%s", sentence[m]);
+        printf(" ");
+        m--;
+        if (m == 0) 
+            printf("%s%c\n", sentence[m], ch);
+    }
+
+    return 0;
+}
+
+
+
+
+
+        
+
+    
 
     
